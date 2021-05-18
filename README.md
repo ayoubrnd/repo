339017658deda6f04b780847b55da85444d3dee1
ayoubrnd <18307015+ayoubrnd@users.noreply.github.com>
2021-05-18 21:06:10 +0200

    Clear

--- README.md
+++
@@ -1,3 +0,0 @@
-# Simple Git Repo
-
-description
--- index.js
+++
@@ -1,8 +0,0 @@
-//sample JS File
-
-const API_KEY = "{SECURE_API_KEY}";
-const makeAPICall = function(key) {
-	console.log("Making API Call...");
-}
-makeAPICall(API_KEY);
-
